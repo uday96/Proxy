58806ec8465956b08b5b4e25aceb1c102359e4f4
Uday <udayteja96@gmail.com>
2017-02-25 12:59:30 +0530

    db settings

--- ForGreaterGood/settings.py
+++ ForGreaterGood/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ForGreaterGood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'proxydb',
+        'USER': 'proxyuser',
+        'PASSWORD': 'forgreatergood',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
