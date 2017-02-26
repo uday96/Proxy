1600d952c3822673c60d7046ff49a6b8b5cdab50
Saikrishna_NV <saikrishna.11597@gmail.com>
2017-02-26 02:50:43 +0000

    redirect AddCourse addstudents

--- prof/urls.py
+++ prof/urls.py
@@ -5,7 +5,7 @@ from photo.views import UploadClassPhotos
 
 urlpatterns = [
     url(r'^homePage/(?P<email_id>[A-Za-z0-9@.]+)/$', views.homePage, name='homePage'),
-    url(r'^course/(?P<course_id>[A-Za-z0-9]+)/$', views.showCourse, name='showCourse'),
+    url(r'^course/(?P<course_info>[A-Za-z0-9,]+)/$', views.showCourse, name='showCourse'),
     url(r'^addCourse/$',views.AddCourse.as_view(),name='addCourse'),
     url(r'^addStudents/(?P<course_info>[A-Za-z0-9,]+)/$',views.AddStudents.as_view(),name='addStudents'),
     url(r'^uploadPhoto/(?P<course_info>[A-Za-z0-9,]+)/$',UploadClassPhotos.as_view(),name='uploadPhoto'),
--- prof/views.py
+++ prof/views.py
@@ -30,8 +30,17 @@ class AddCourse(View):
             profEmail = request.session['email']
             prof = Users.objects.get(email=profEmail)
             create_person_group(profEmail,name,year)
+            
+            alert('Success')
+            try:
+                courseList = Course.objects.filter(profID=prof.ID)
+                context = {'course_list': courseList, 'prof' : prof}
+                return render(request, 'prof/homepage.html', context)
+            except:
+                print "Error"
+            return HttpResponse("Error")
             # Call the function to create PersonGroup Microsoft API
-            return HttpResponse("courseID, year, profID: " + str(courseID) + "," + str(year) + "," + str(prof.ID))
+            # return HttpResponse("courseID, year, profID: " + str(courseID) + "," + str(year) + "," + str(prof.ID))
         else:
             return HttpResponse("Error")
 
@@ -48,19 +57,22 @@ class AddStudents(View):
         form = StudentAddForm(request.POST)
         if form.is_valid():
             print 'valid form'
-            # name = form.cleaned_data['name']
-            # courseID = form.cleaned_data['courseID']
-            # year = form.cleaned_data['year']
-            # room = form.cleaned_data['room']
             profEmail = request.session['email']
             prof = Users.objects.get(email=profEmail)
             studentIDs = str(form.cleaned_data['studentIDs']).split(',')
             [course_id, year] = str(course_info).split(',')
             create_person(course_id,year,studentIDs)
-            print request.POST
-            # courseID = request.POST.get('courseID')
+            alert('Success')
+            # print request.POST
             # Call the function to create PersonGroup Microsoft API
-            return HttpResponse("courseID, studentIDs, profID: " + str(course_id) + "," + str(studentIDs) + "," + str(prof.ID))
+            course = Course.objects.get(courseID=course_id, year=year)
+            try:
+                context = {'course': course, 'prof' : prof}
+                return render(request, 'prof/coursepage.html', context)
+            except:
+                print "Error"
+            return HttpResponse("Error")
+            # return HttpResponse("courseID, studentIDs, profID: " + str(course_id) + "," + str(studentIDs) + "," + str(prof.ID))
         else:
             return HttpResponse("Error")
             
@@ -78,10 +90,11 @@ def homePage(request, email_id):
     # return render(request, 'prof/homepage.html', context)
     return HttpResponse("Error")
 
-def showCourse(request, course_id):
+def showCourse(request, course_info):
     # return HttpResponse("Hello, world. You're at the prof index page.")
     # print course_id
-    course = Course.objects.get(courseID=course_id)
+    [course_id, year] = str(course_info).split(',')
+    course = Course.objects.get(courseID=course_id, year=year)
     # print course.profID
     # courseList = Course.objects.filter(profID=prof.profID)
     try:
